4ff5e7a8aae8a2a0477ccee7fdd5755dcb5f79c5
sivatharun2212 <sivatharun2212@gmail.com>
2023-05-14 10:29:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./index.css";
+const uRL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
 
-function App() {
+export default function App() {
+  const [drinksdata, setDrinksdata] = useState([]);
+  const [searchDrink, setSearchDrink] = useState("");
+
+  const fetchDrinks = async (apiURL) => {
+    const response = await fetch(apiURL);
+    const { drinks } = await response.json();
+    setDrinksdata(drinks);
+  };
+
+  const searchedUrl = `${uRL}${searchDrink}`;
+
+  useEffect(() => {
+    fetchDrinks(searchedUrl);
+  }, [searchedUrl]);
+
+  const handleSearchChange = (e) => {
+    setSearchDrink(e.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="searchdiv">
+        <form>
+          <input
+            type="text"
+            name="search"
+            id="search"
+            placeholder="search drink..."
+            onChange={handleSearchChange}
+          />
+        </form>
+      </div>
+      <hr />
+      <div className="container">
+        {drinksdata !== null && drinksdata.length > 0 ? (drinksdata.map((drink) => {
+          const { strDrink, strDrinkThumb, idDrink } = drink;
+          return (
+            <div key={idDrink} className="card">
+              <div className="cardImg">
+                <img src={strDrinkThumb} alt="" />
+              </div>
+              <div className="cardTitle">
+                <h3>{strDrink}</h3>
+              </div>
+            </div>
+          );
+        })) :
+        (<h2>No Drinks Found!</h2>)}
+      </div>
     </div>
   );
 }
-
-export default App;
